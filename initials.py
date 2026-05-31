e4f5b8498d1474c3a396eea1a42811361b3d0f8a
alvintmartin <alvintachiemartin4@gmail.com>
2026-05-31 22:08:35 +0000

    initials.py
    STUDENT ID: 2425402990
    NAME: Alvin Tachie- Martin

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Alvin Tachie - Martin
+# STUDENT ID: 2425402990
+# Email: 2425402990@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Alvin Tachie - Martin, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large styled large letters
 
+    print()
+    print("...ATM")
+    print()
+
+    print("...AAAAAAAAAAAA........TTTTTTTTTTTT........MMMM......MM")
+    print("...AAAA....AAAA........TTTTTTTTTTTT........MMMMM....MMM")
+    print("...AAAA....AAAA...........TTTTTT...........MMMMMM..MMMM")
+    print("...AAAA....AAAA...........TTTTTT...........MM.MMMMMM.MM")
+    print("...AAAAAAAAAAAA...........TTTTTT...........MM..MMMM..MM")
+    print("...AAAAAAAAAAAA...........TTTTTT...........MM...MM...MM")
+    print("...AAAA....AAAA...........TTTTTT...........MM........MM")
+    print("...AAAA....AAAA...........TTTTTT...........MM........MM")
+    print("...AAAA....AAAA...**......TTTTTT......**...MM........MM")
+    print("...AAAA....AAAA...**......TTTTTT......**...MM........MM")
+
+    print()
 
 
 main()
+
+
+
